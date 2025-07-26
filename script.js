6ab8bbc84a6b995c4ad5f9c74928d32cad01d510
AryanS6000 <163299006+AryanS6000@users.noreply.github.com>
2025-07-26 11:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 //your JS code here. If required.
-window.onload = function(){
-	const select = document.getElementById("colorSelect");
-	const button = document.querySelector('input[type = "button"]');
+window.onload = function () {
+  const select = document.getElementById("colorSelect");
+  const button = document.querySelector('input[type="button"]');
 
-	button.onclick = function(){
-		const slectedIndex = select.selectedIndex;
-		if(selectedIndex !== -1){
-			select.remove(selectedIndex);
-		}
-	};
+  button.onclick = function () {
+     
+    const selectedIndex = select.selectedIndex;
+
+     
+    if (selectedIndex !== -1) {
+      select.remove(selectedIndex);
+    }
+  };
 };
